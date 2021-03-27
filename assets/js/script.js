e09afc3531abed9eeaa573c30c0d79f06efba59e
japankid-code <japankid.jake@gmail.com>
2021-03-27 09:52:54 -0500

    users scores now save and load from local

--- assets/js/script.js
+++ assets/js/script.js
@@ -84,12 +84,12 @@ let questionList = [
 ]
 
 // create timer function for remaining time
-let countdown = function() {
+const countdown = function() {
     timeLeft = localStorage.getItem("timeLeft")
     let timeInterval = setInterval(function() { // starts the timer
         if (questionList.length > 0) {
             if (timeLeft > 1) {
-                // Set the `textContent` of `timerEl` to show the remaining seconds
+                // Set  the `textContent` of `timerEl` to show the remaining seconds
                 timerEl.textContent = `${timeLeft} seconds remaining.`;
                 timeLeft--;
                 saveTimeLeft(timeLeft); // save to localStorage after update
@@ -115,7 +115,7 @@ let countdown = function() {
 // create intro function that displays the rules using questionList[0] properties
     //pressing start will run startGame()
 
-let renderQuestion = function(q) {
+const renderQuestion = function() {
     questionEl.textContent = questionList[0].question;
     emptyChildren(answerEl);
     // generate buttons for each answer in answers
@@ -133,18 +133,20 @@ let renderQuestion = function(q) {
 }
 
 // create startGame() containing game logic
-let startGame = function(e) {
+const startGame = function(e) {
     answerEl.removeEventListener("submit", startGame);
     // fill in first prompt and answer buttons from questionList
     renderQuestion(e);
 }
 
-let nextRound = function(e) {
+const nextRound = function(e) {
     let targetEl = e.target;
     // calculate user score here using e.target, comparing to answerList (the buttons)
     answerList = Array.from(answerEl.children)
-    // checking the first question
-    if (targetEl === answerList[1]) {
+    let correctAnswer = questionList[0].correct;
+    // using object property passed in as index to compare :) ty troy
+    if (targetEl.textContent === questionList[0].answers[correctAnswer]) {
+        console.log
         questionList.shift();
         renderQuestion(questionList);
     } else {
@@ -154,23 +156,42 @@ let nextRound = function(e) {
     }
 }
 
-let submitScore = function(e) {
+const submitScore = function(e) {
     emptyChildren(questionSection);
-    questionSection.insertAdjacentHTML(
-        `afterbegin`,
-        `<p>add your initials here and submit the score!!</p>
-        <button id="submit-score">submit score</button>
-        </form>
-        <div class="divider"></div>
-        <em><span id="result">great work!</span></em>`
-    );
+    // fill in the submit form with 
+    let submitP = document.createElement("h3");
+    submitP.textContent = `Your score was ${localStorage.getItem("timeLeft")}!
+                           add your initials here and submit the score!!`;
+    let submitInput = document.createElement("input");
+    submitInput.setAttribute("id", "user-initials");
+    let submitButton = document.createElement("button");
+    submitButton.setAttribute("id", "submit-score");
+    submitButton.textContent = `submit score`;
+    let submitForm = document.createElement("form");
+    submitForm.append(submitInput, submitButton);
+    submitButton.addEventListener("click", function(e) {
+        e.preventDefault();
+        let currentScore = {
+            initials: submitInput.value,
+            score: timeLeft,
+        }
+        console.log(userScores);
+        console.log(currentScore);
+        userScores.push(currentScore);
+        localStorage.setItem("localScores", JSON.stringify(userScores));
+    });
+    questionSection.append(submitP, submitForm);
 }
 
-let saveTimeLeft = function(value) {
+const loadScores = function() {
+    userScores = JSON.parse(localStorage.getItem("localScores"));
+}
+
+const saveTimeLeft = function(value) {
     localStorage.setItem("timeLeft", value);
 }
 
-let timeSkip = function() {
+const timeSkip = function() {
     let timeLeft = localStorage.getItem("timeLeft");
     timeLeft -= 10;
     // debugger;
@@ -180,9 +201,9 @@ let timeSkip = function() {
 }
 
 // create a buttonHandler() to preventdefault on all buttons
-var buttonHandler = function(e) {
+const buttonHandler = function(e) {
     e.preventDefault();
-    let timeLeft = 100;
+    let timeLeft = 63;
     localStorage.setItem("timeLeft", timeLeft);
     descriptionP.remove();
     countdown(timeLeft);
@@ -192,3 +213,4 @@ var buttonHandler = function(e) {
 // create event listener
     // add function to delegate clicks to buttons by data-type-id
 readyButton.addEventListener("click", buttonHandler);
+loadScores();
